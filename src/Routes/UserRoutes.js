34f34b11a3c931fa37f52ffa401650a45f5b6db2
WillianWCR <willian@willianwcr.com.br>
2020-08-09 16:51:00 -0300

    Update UserRoutes.js

--- src/Routes/UserRoutes.js
+++ src/Routes/UserRoutes.js
@@ -2,25 +2,19 @@ const express = require('express');
 const routes = express.Router();
 const UserController = require('../Controllers/UserController');
 
+// List all users
+routes.get('/', UserController.index);
+
 // Register new User
 routes.post('/', UserController.store);
 
 // Retrieve a user
 routes.get('/:id', UserController.showById);
 
-// Retrieve a user by email
-routes.get('email/:email', UserController.showByEmail);
-
-// Retrieve a user by username
-routes.get('username/:username', UserController.showByUsername);
-
 // Update user
 routes.put('/:id', UserController.update);
 
 // Delete user
 routes.delete('/:id', UserController.delete);
-
-// List all users
-routes.get('/', UserController.index);
 
 module.exports = routes;
